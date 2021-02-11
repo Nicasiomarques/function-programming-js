ac14672c9c915f79ea17003366309792ea037538
nicasiomarques <nicasiomarques>
2021-02-11 19:17:26 +0100

    ex2 currying

--- basic/currying.js
+++ basic/currying.js
@@ -1,7 +1,13 @@
 import { curry } from 'ramda';
 
-const sum = (a, b) => a + b;
+// const sum = (a, b) => a + b;
 
-const curriedSum = curry(sum);
+// const curriedSum = curry(sum);
 
-console.log(curriedSum(10)(20));
+// console.log(curriedSum(10)(20));
+
+const arrayObjects = [{ id: 1 }, { id: 6 }, { id: 12 }, { id: 449 }];
+
+// 1ª way:
+const ids = arrayObjects.map(item => item.id);
+console.log(ids);
