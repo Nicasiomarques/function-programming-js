39a43d914f7f3d365f85b27bc73c9f8a16d03cc3
nicasiomarques <nicasiomarques>
2021-02-12 20:30:04 +0100

    composition refactor

--- ramda/5.composite.js
+++ ramda/5.composite.js
@@ -1,9 +1,14 @@
-const toSlug = text =>
-  encodeURIComponent(
-    text
-      .split(' ')
-      .map(w => w.toLowerCase())
-      .join('-')
-  );
+import { split, toLower, map, join } from 'ramda';
+import { compose } from 'ramda';
+
+// const toSlug = text =>
+//   encodeURIComponent(
+//     text
+//       .split(' ')
+//       .map(w => w.toLowerCase())
+//       .join('-')
+//   );
+
+const toSlug = compose(encodeURIComponent, join('-'), map(toLower), split(' '));
 
 console.log(toSlug('branca de neve e os 6 anoes'));
