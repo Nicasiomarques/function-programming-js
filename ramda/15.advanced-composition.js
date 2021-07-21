fcd6beb86f0e4d7a0f790ce4e35655956dcef587
Nicasiomarques <nicasiomarques18@gmail.com>
2021-07-21 12:05:49 +0100

    add updateTemperature

--- ramda/15.advanced-composition.js
+++ ramda/15.advanced-composition.js
@@ -4,3 +4,8 @@ const percentil = require('./percentil')
 
 const kelvinToCelsius = k => k - 272.15
 const kelvinToFahrenheit = k => k * 9 / 5 - 459.67
+
+const updateTemperature = R.curry((convertFn, city) => {
+  const temperature = Math.round(convertFn(city.temperature))
+  return R.merge(city, { temperature })
+})
